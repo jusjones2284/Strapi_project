f3febc327a4244e7181daf206779cd5e6d19329b
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-08-01 23:17:44 -0400

    created a couple new components and worked on css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useGlobalContext } from "./Context";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
 
 const App = () => {
 
-const { isSidebarOpen } = useGlobalContext();
-  console.log(isSidebarOpen);
 
-  return <h2>Strapi Starter</h2>;
+
+  return <main>
+            <Navbar />
+            <Hero />
+            <Submenu />
+            <Sidebar />
+  </main>
   
 };
 export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { FaBars } from "react-icons/fa"
+import { useGlobalContext } from "./Context"
 
 const Navbar = () => {
+ const { openSidebar } = useGlobalContext();
+ 
   return (
-    <h2>Navbar</h2>
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">straspi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+      </div>
+    </nav>
   )
 }
 
