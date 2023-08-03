3ec31faa63c4cc23a67aea80d1c180ffe9df5260
Justin Jones <98709555+jusjones2284@users.noreply.github.com>
2023-08-02 21:02:54 -0400

    created a new component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { FaTimes } from "react-icons/fa"
+import sublinks from "./data"
+import { useGlobalContext } from "./Context"
 
 const Sidebar = () => {
+  const {isSidebarOpen, closeSidebar } = useGlobalContext()
+  
   return (
-    <h2>Sidebar</h2>
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes /> 
+        </button>
+        <div className="sidebar-links">
+          {}
+        </div>
+      </div>
+    </aside>
   )
 }
 
